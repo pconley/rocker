42665949da2d110e25362f65b2aaae6b42334a7e
Pat Conley <pat.conley@expel.io>
2020-01-04 08:51:23 -0500

    Chapter 6+

--- app/controllers/bands.js
+++ app/controllers/bands.js
@@ -1,9 +1,12 @@
 import Controller from '@ember/controller';
 import { action } from '@ember/object';
 import { empty } from '@ember/object/computed';
+import { inject as service } from '@ember/service';
+
 import Band from 'rocker/models/band';
 
 export default Controller.extend({
+  router: service(),
   isAddingBand: false,
   isBandNameEmpty: empty('newBandName'),
   newBandName: '',
@@ -13,9 +16,11 @@ export default Controller.extend({
     event.preventDefault();
     let newBand = Band.create({ name: this.newBandName });
     this.model.pushObject(newBand);
-    this.set('newBandName', '');
-    // this.set('isAddingBand',false);
+    this.setProperties({
+      newBandName: '',
+      isAddingBand: false
+    });
+    newBand.set('slug', dasherize(newBand.name));
+    this.router.transitionTo('bands.band.songs', newBand.slug);
   }),
 });
-
-
