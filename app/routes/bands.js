f6e5ee2de93a99bcb154239e83ce168b4e1f5d32
Pat Conley <pat.conley@expel.io>
2020-01-12 14:25:03 -0500

    Chapter 12 part1

--- app/routes/bands.js
+++ app/routes/bands.js
@@ -1,7 +1,13 @@
 import Route from '@ember/routing/route';
+// import wait from 'rocker/utils/wait';
 
 export default Route.extend({
   
+  // async model() {
+  //   await wait(3000);
+  //   return this.store.findAll('band');
+  // }
+
   model() {
     return this.store.findAll('band');
   }
--- app/routes/bands/band/songs.js
+++ app/routes/bands/band/songs.js
@@ -1,6 +1,12 @@
 import Route from '@ember/routing/route';
+// import wait from 'rocker/utils/wait';
 
 export default Route.extend({
+  // async model() {
+  //   await wait(2000);
+  //   return this.modelFor('bands.band');
+  // },
+
   model() {
     return this.modelFor('bands.band');
   },
